35d81fb5907807fa76489c0a8bd6a7c5097213ce
mb3ch3 <rhowdan@gmail.com>
2022-06-20 11:35:46 +0300

    from local

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+//import { Hero } from '../hero';
+import { HEROES } from '../mockheroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+}
---
+++ src/app/mockheroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id:12, name:'aaa', power:'jdnvcjcdss'},
+    {id:14, name:'nbb', power:'jdnvkjcnsns'},
+    {id:1, name:'dff', power:'jowiqodnvs'},
+    {id:19, name:'wew', power:'isnnjdnvs'},
+    {id:9, name:'jdj', power:'jijcoisjcdnvs'},
+    {id:10, name:'mdc', power:'jiuhiednvs'},
+    {id:66, name: 'uhe', power:'nconciucd'}
+]
